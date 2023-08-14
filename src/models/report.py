8f3bf0ffdfc7053eb925bb0e8e92ee6ab36bfe0d
cu3v4n4 <lastfpvshaman009@gmail.com>
2023-08-13 17:53:06 -0700

    "updated code"

--- src/models/report.py
+++
@@ -1,9 +0,0 @@
-# from src.extensions import db
-# from datetime import datetime
-
-# class Report(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     manager = db.relationship('Manager', backref='reports', lazy=True)
-#     type = db.Column(db.String(50), nullable=False)
-#     result = db.Column(db.Text, nullable=False)
-#     generated_date = db.Column(db.DateTime, default=datetime.now())
