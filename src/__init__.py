617f7e8fa6b84e45d83d697ff1f67d9187d9f164
Shawn <68162598+Shawn0779@users.noreply.github.com>
2023-08-15 03:14:53 -0700

    Python Documentation (Updated)

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,38 @@ from flask import Flask,jsonify
 from src.extensions import db
 from flask_jwt_extended import JWTManager
 from config import Config
-"""This is a factory function that controls everything that happens in the app"""
+"""Factory Function for Creating Flask Application
+
+This module contains a factory function `create_app` that is responsible for creating and configuring a Flask application for the HotelBackend project. The application is configured using the provided `Config` class and is set up with necessary extensions and blueprints.
+
+Functions:
+    create_app(test_config=Config): 
+        This function creates and configures a Flask application instance. It sets up the application's configuration, initializes the database using SQLAlchemy, integrates JWT authentication using Flask-JWT-Extended, and registers a main blueprint from the `src.main` module. The main blueprint likely contains routes and views for the core functionality of the application.
+
+    Parameters:
+        test_config (class, optional): 
+            An optional configuration class to use when creating the application. The default is the `Config` class provided in the configuration module.
+
+Returns:
+    app (Flask):
+        The configured Flask application instance ready to be run.
+
+Note:
+    - The `__init__.py` file serves as a package initializer and is automatically imported when the `src` module is imported.
+    - The `Config` class holds configuration settings for the application.
+    - The application factory pattern allows for easier testing and flexibility in configuration changes by creating separate instances of the application.
+    - The factory function initializes the database, JWT authentication, and blueprints to structure the application's components.
+
+Example Usage:
+    # Creating the Flask application using the default configuration
+    from app import create_app
+    app = create_app()
+    app.run()
+
+    # Creating the Flask application with a custom configuration
+    from config import DevelopmentConfig
+    app = create_app(test_config=DevelopmentConfig)
+    app.run()"""
 def create_app(test_config=Config):
     
     app = Flask(__name__, instance_relative_config=True)
@@ -24,9 +55,8 @@ def create_app(test_config=Config):
     JWTManager(app)
     app.register_blueprint(main_bp)
   
-    #app.register_blueprint(hotel) 
+    #app.register_blueprint(hotel)""
    
 
     return app
-print(help(create_app))
         
